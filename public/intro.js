81506255d356f726c2eaf7a7e87ee55d65fff736
John Mark Lumada <jmlumada2004@gmail.com>
2025-10-29 09:43:34 +0800

    changes

--- public/intro.js
+++ public/intro.js
@@ -20,7 +20,7 @@ const API_URL =
 
 // --- Typewriter Effect ---
 const input = document.getElementById("searchInput");
-const phrases = ["Filipiniana","General Education","Accountancy","Computer Studies","Criminology","Education","Engineering","Commerce","Maritime","Mass Communication","Political Science", "Psychology", "Aviation", "Fiction", "Reference", "Periodical"];
+const phrases = ["Filipiniana","General Education","Accountancy","Computer Studies","Criminology","Education","Engineering","Commerce","Maritime","Mass Communication","Political Science", "Psychology", "Aviation", "Fiction", "Reference", "Periodical", "Senior High School"];
 let phraseIndex = 0, charIndex = 0, deleting = false;
 
 function typeAnimation() {
